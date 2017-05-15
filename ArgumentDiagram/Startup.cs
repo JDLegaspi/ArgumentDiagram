c25ab0fbd4ed1ae4964582b4e13b9ff8edfa48ac
Daniel <jdalegaspi@gmail.com>
2017-05-15 12:01:38 +1000

    Created forms for arguments

--- ArgumentDiagram/Startup.cs
+++ ArgumentDiagram/Startup.cs
@@ -53,7 +53,7 @@ namespace ArgumentDiagram
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=ArgumentDiagram}/{action=Index}/{id?}");
             });
         }
     }
