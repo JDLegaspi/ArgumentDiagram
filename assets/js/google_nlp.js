3d3081a96c83bf50913574b3031cbb684776966a
JDLEGASPI-PC\JD Legaspi <jdalegaspi@gmail.com>
2017-10-03 22:54:58 +1000

    more nlp

--- assets/js/google_nlp.js
+++ assets/js/google_nlp.js
@@ -36,37 +36,63 @@ function processRootWords(json) {
     var tokens = json.tokens;
     var root;
     var rootTokenIndex;
-    var string = "";
-
-    for (var i = 0; i < tokens.length; i++) {
-        if (getLabel(tokens[i]) == 'ROOT') {
-            root = tokens[i]['text']['content'];
-            rootTokenIndex = i;
-            break;
-        }
-    }
-
-    for (var i = 0; i < tokens.length; i++) {
-        if (isDependant(tokens[i], rootTokenIndex)) {
-            if (getLabel(tokens[i]) == "CC" && i > rootTokenIndex) break;
-            if (getLabel(tokens[i]) == "AUX" || getLabel(tokens[i]) == "NSUBJ" || getLabel(tokens[i]) == "TMOD" || getLabel(tokens[i]) == "DOBJ" || getLabel(tokens[i]) == "NEG" || getLabel(tokens[i]) == "ROOT") {
-                if (getLabel(tokens[i]) == "DOBJ" || getLabel(tokens[i]) == "TMOD") {
-                    for (var j = 0; j < tokens.length; j++) {
-                        if (isDependant(tokens[j], i) && getLabel(tokens[j]) == "POSS") {
-                            string += " " + tokens[j]['text']['content'] + "'s";
-                            break;
-                        }
-                    }   
-                }     
-                if (!(tokens[i]['text']['content'] == "n't" || tokens[i]['text']['content'] == "'s'" || tokens[i]['text']['beginOffset'] == 0)) {
-                    string += " ";
-                }        
-                string += tokens[i]['text']['content'];
+    var sentences = [];
+    
+    loop1:
+    for (var x = 0; x < json['sentences'].length; x++) {
+        var string = "";
+        var offset = json['sentences'][x]['text']['beginOffset'];
+        console.log(offset);
+        var sentenceIndex;
+        
+        loopFindOffset:
+        for (var i = 0; i < tokens.length; i++) {
+            if (tokens[i]['text']['beginOffset'] == offset) {
+                sentenceIndex = i;
+                break loopFindOffset;
             }
         }
-    }
 
-    return string;
+        loop2:
+        for (var i = sentenceIndex; i < tokens.length; i++) {
+            if (getLabel(tokens[i]) == 'ROOT') {
+                root = tokens[i]['text']['content'];
+                rootTokenIndex = i;
+                break loop2;
+            }
+        }
+        
+        loop3:
+        for (var i = sentenceIndex; i < tokens.length; i++) {
+            if (isDependant(tokens[i], rootTokenIndex)) {
+                if (getLemma(tokens[i]) == ".") {
+                    break loop3;
+                }
+                if (getLabel(tokens[i]) == "CC" && i > rootTokenIndex) break loop3;
+                if (getLabel(tokens[i]) == "AUX" || getLabel(tokens[i]) == "NSUBJ" || getLabel(tokens[i]) == "TMOD" || getLabel(tokens[i]) == "DOBJ" || getLabel(tokens[i]) == "NEG" || getLabel(tokens[i]) == "ROOT") {
+                    if (getLabel(tokens[i]) == "DOBJ" || getLabel(tokens[i]) == "TMOD") {
+                        
+                        loop4:
+                        for (var j = sentenceIndex; j < tokens.length; j++) {
+                            if (isDependant(tokens[j], i) && getLabel(tokens[j]) == "POSS") {
+                                string += " " + tokens[j]['text']['content'] + "'s";
+                                break loop4;
+                            }
+                        }   
+                    }     
+                    if (!(tokens[i]['text']['content'] == "n't" || tokens[i]['text']['content'] == "'s'" || tokens[i]['text']['beginOffset'] == 0)) {
+                        string += " ";
+                    }        
+                    string += tokens[i]['text']['content'];
+                }
+            }
+        }
+        sentences[x] = string;
+
+
+    }
+    return sentences;
+    //return string;
 }
 
 function isDependant(token, headTokenIndex) {
@@ -75,4 +101,8 @@ function isDependant(token, headTokenIndex) {
 
 function getLabel(token) {
     return token['dependencyEdge']['label'];
+}
+
+function getLemma(token) {
+    return token['lemma'];
 }
