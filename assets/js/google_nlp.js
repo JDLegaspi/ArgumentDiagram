34d5046200d37af2b6aeb74c20617f01c7962ec7
JDLEGASPI-PC\JD Legaspi <jdalegaspi@gmail.com>
2017-10-23 16:54:19 +1000

    Improved NLP algorithm

--- assets/js/google_nlp.js
+++ assets/js/google_nlp.js
@@ -86,7 +86,7 @@ function processRootWords(json) {
                 }
                 if (getLabel(tokens[i]) == "CC" && i > rootTokenIndex) break loop3;
                 
-                if (getLabel(tokens[i]) == "AUX" || getLabel(tokens[i]) == "NSUBJ" || getLabel(tokens[i]) == "TMOD" || getLabel(tokens[i]) == "DOBJ" || getLabel(tokens[i]) == "NEG" || getLabel(tokens[i]) == "ROOT" || getLabel(tokens[i]) == "XCOMP" || getLabel(tokens[i]) == "ACOMP" || getLabel(tokens[i]) == "AUXPASS" || getLabel(tokens[i]) == "NSUBJPASS" || getLabel(tokens[i]) == "ATTR") {
+                if (getLabel(tokens[i]) == "AUX" || getLabel(tokens[i]) == "NSUBJ" || getLabel(tokens[i]) == "TMOD" || getLabel(tokens[i]) == "DOBJ" || getLabel(tokens[i]) == "NEG" || getLabel(tokens[i]) == "ROOT" || getLabel(tokens[i]) == "XCOMP" || getLabel(tokens[i]) == "ACOMP" || getLabel(tokens[i]) == "AUXPASS" || getLabel(tokens[i]) == "NSUBJPASS" || getLabel(tokens[i]) == "ATTR" || (isDependant(tokens[i], rootTokenIndex) && getLabel(tokens[i]) == "ADVCL")) {
                     
                     if (getLabel(tokens[i]) == "DOBJ" || getLabel(tokens[i]) == "TMOD") {
                         
@@ -98,22 +98,31 @@ function processRootWords(json) {
                                 break loop4;
                             }
                         }
-
                     } 
+                    
 
                     if (getLabel(tokens[i]) == "ATTR") {
                         loop5:
                         for (var j = sentenceIndex; j <tokens.length; j++) {
-                            if (isDependant(tokens[j], i) && getLabel(tokens[j]) == "AMOD" || isDependant(tokens[j], i) && getLabel(tokens[j]) == "NN") {
+                            if (isDependant(tokens[j], i) && getLabel(tokens[j]) == "AMOD" || isDependant(tokens[j], i) && getLabel(tokens[j]) == "PREP" || isDependant(tokens[j], i) && getLabel(tokens[j]) == "NN") {
                                 string += " " + tokens[j]['text']['content'];
                                 output.push(outputStartAndEnd(tokens[j]['text']['beginOffset'], tokens[j]['text']['content']));
+
+                                if (getLabel(tokens[j]) == "PREP") {
+                                    for (var k = 0; k < tokens.length; k++) {
+                                        if (isDependant(tokens[k], j) && getLabel(tokens[k]) == "POBJ") {
+                                            output.push(outputStartAndEnd(tokens[k]['text']['beginOffset'], tokens[k]['text']['content']));
+                                        }
+                                    }
+                                }
+
                             }
                         }
                     }
 
                     if (getLabel(tokens[i]) == "NSUBJ") {
                         for (var j = sentenceIndex; j <tokens.length; j++) {
-                            if (isDependant(tokens[j], i) && getLabel(tokens[j]) == "POSS") {
+                            if (isDependant(tokens[j], i) && getLabel(tokens[j]) == "POSS" || isDependant(tokens[j], i) && getLabel(tokens[j]) == "AMOD") {
                                 string += " " + tokens[j]['text']['content'];
                                 output.push(outputStartAndEnd(tokens[j]['text']['beginOffset'], tokens[j]['text']['content']));
                             }
@@ -122,7 +131,23 @@ function processRootWords(json) {
                     
                     if (!(tokens[i]['text']['content'] == "n't" || tokens[i]['text']['content'] == "'s'" || tokens[i]['text']['beginOffset'] == 0)) {
                         string += " ";
-                    }        
+                    }
+
+                    if (getLabel(tokens[i]) == "ADVCL") {
+                        for (var j = sentenceIndex; j <tokens.length; j++) {
+                            if (isDependant(tokens[j], i) && getLabel(tokens[j]) == "NSUBJ" || isDependant(tokens[j], i) && getLabel(tokens[j]) == "MARK") {
+                                string += " " + tokens[j]['text']['content'];
+                                output.push(outputStartAndEnd(tokens[j]['text']['beginOffset'], tokens[j]['text']['content']));
+                                if (getLabel(tokens[j]) == "NSUBJ") {
+                                    for (var k = 0; k < tokens.length; k++) {
+                                        if (isDependant(tokens[k], j) && getLabel(tokens[k]) == "POSS") {
+                                            output.push(outputStartAndEnd(tokens[k]['text']['beginOffset'], tokens[k]['text']['content']));
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
                     
                     string += tokens[i]['text']['content'];
                     output.push(outputStartAndEnd(tokens[i]['text']['beginOffset'], tokens[i]['text']['content']));
@@ -130,7 +155,7 @@ function processRootWords(json) {
                     if (getLabel(tokens[i]) == "XCOMP") {
                         loop5:
                         for (var j = sentenceIndex; j <tokens.length; j++) {
-                            if (isDependant(tokens[j], i) && getLabel(tokens[j]) == "DOBJ") {
+                            if (isDependant(tokens[j], i) && getLabel(tokens[j]) == "DOBJ" || isDependant(tokens[j], i) && getLabel(tokens[j]) == "AUX" || isDependant(tokens[j], i) && getLabel(tokens[j]) == "ACOMP" || isDependant(tokens[j], i) && getLabel(tokens[j]) == "ATTR") {
                                 string += " " + tokens[j]['text']['content'];
                                 output.push(outputStartAndEnd(tokens[j]['text']['beginOffset'], tokens[j]['text']['content']));
                             }
